54b7d4a704bb27312626e9cc2ba9f709f29875a0
shinleon <shinleon@uw.edu>
2020-12-20 17:29:20 -0800

    adjusted the responses to the query to intgers from [-1:2] inclusive instead of 1 to 4

--- src/process_data.py
+++ src/process_data.py
@@ -2,11 +2,20 @@ import pandas
 import os
 
 # over write all staff requirements to -2 - 2 scale
+# use lower case y for two year encoding
 # 1 -> -1
 # 2 -> 0
 # 3 -> 1
 # 4 -> 2
 # -2 reserved for unreal shifts
 print(os.getcwd())
-m = pandas.read_excel("../data/staff_requirements.xlsx")
+m = pandas.read_excel("data/staff_requirements.xlsx")
+
+print(type(m.loc[1]["Sunday"]))
+
+for i in range(0, 10):          # ranges over the rows
+    for column_head in m.columns[1:]: # ranges over the columns
+        # print(m.loc[i][column_head], i, column_head)
+        m.loc[i][column_head] = int(m.loc[i][column_head][0]) - 2
+
 print(m)
