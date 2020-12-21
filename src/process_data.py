30b74ae8fb3aaa6ad7e340d0f87a30157b0b4659
shinleon <shinleon@uw.edu>
2020-12-20 17:34:28 -0800

    made it so the scaled staff reqirements gets output to an .xlsx file

--- src/process_data.py
+++ src/process_data.py
@@ -8,14 +8,14 @@ import os
 # 3 -> 1
 # 4 -> 2
 # -2 reserved for unreal shifts
-print(os.getcwd())
+# print(os.getcwd())
 m = pandas.read_excel("data/staff_requirements.xlsx")
 
-print(type(m.loc[1]["Sunday"]))
+# print(type(m.loc[1]["Sunday"]))
 
 for i in range(0, 10):          # ranges over the rows
     for column_head in m.columns[1:]: # ranges over the columns
         # print(m.loc[i][column_head], i, column_head)
         m.loc[i][column_head] = int(m.loc[i][column_head][0]) - 2
 
-print(m)
+m.to_excel("data/scaled_staff_req.xlsx")
