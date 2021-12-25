5059284245a7d7d562782dc4e49f7d0bec67bce3
Winston Chen <chenwt@uw.edu>
2021-12-25 11:33:20 -0800

    update utils code

---
+++ src/common_args.py
@@ -0,0 +1,43 @@
+
+def get_shift_req_args(parser):
+    parser.add_argument(
+        "--start_date", type=str, help="shift start date(inclusive), format: mm/dd/yy",
+        required=True
+    )
+    parser.add_argument(
+        "--end_date", type=str, help="shift end date(inclusive), format: mm/dd/yy",
+        required=True
+    )
+    parser.add_argument(
+        "--excluding_dates", type=str, nargs='+',
+        help="dates to exclude from shift requirement, format: mm/dd/yy",
+        default=[]
+    )
+    parser.add_argument(
+        "--max_num_shifts", type=int, help="maximum number of shifts per day",
+        default=3
+    )
+    parser.add_argument(
+        "--shifts_names", type=str, nargs='+',
+        help="shift names, default: Primary, Secondary, Daytime",
+        default=["Primary", "Secondary", "Daytime"]
+    )
+    parser.add_argument(
+        "--weekday_num_shifts", type=int, help="number of shifts per weekday",
+        default=2
+    )
+    parser.add_argument(
+        "--weekend_num_shifts", type=int, help="number of shifts per weekend",
+        default=3
+    )
+    parser.add_argument(
+        "--special_weekdays", type=str, nargs='+',
+        help="weekend days that should be consiedered as weekdays, format: mm/dd/yy",
+        default=[]
+    )
+    parser.add_argument(
+        "--special_weekends", type=int, nargs='+',
+        help="weekdays that should be consiedered as weekends, format: mm/dd/yy",
+        default=[]
+    )
+    return parser
---
+++ src/data_process/utils.py
@@ -0,0 +1,76 @@
+from datetime import datetime, timedelta
+
+def get_date_arr(start_date, end_date, date_format="%m/%d/%y"):
+    """
+    Return a list of dates from start_date to end_date.
+    """
+    date_arr = []
+    start_date = datetime.strptime(start_date, date_format).date()
+    end_date = datetime.strptime(end_date, date_format).date()
+    delta = end_date - start_date
+    for i in range(delta.days + 1):
+        curr_date = start_date + timedelta(days=i)
+        date_arr.append(curr_date.strftime(date_format))
+    return date_arr
+
+
+def validate_datestr(date_str, date_format="%m/%d/%y"):
+    """
+    Validate a date string.
+    """
+    try:
+        datetime.strptime(date_str, date_format)
+        return True
+    except ValueError:
+        return False
+
+def is_weekend(date_str, date_format="%m/%d/%y"):
+    """
+    Return True if the date is a weekend.
+    """
+    date = datetime.strptime(date_str, date_format).date()
+    return date.weekday() >= 5
+
+def is_same_day(date_str1, date_str2, date_format="%m/%d/%y"):
+    """
+    Return True if the two dates are the same.
+    """
+    date1 = datetime.strptime(date_str1, date_format).date()
+    date2 = datetime.strptime(date_str2, date_format).date()
+    return date1 == date2
+
+def contain_same_day(date_str, date_str_arr, date_format="%m/%d/%y"):
+    """
+    Return True if the date_str is in the date_str_arr.
+    """
+    date = datetime.strptime(date_str, date_format).date()
+    for d in date_str_arr:
+        if date == datetime.strptime(d, date_format).date():
+            return True
+    return False
+
+def keep_trail_parentheses_num(s) -> float:
+    """
+    Return number in the trailling parenthese of a string.
+
+    e.g.
+    keep_trail_parentheses("Marry Christmas (24)") -> 24
+    """
+    left_idx = s.rfind("(")
+    right_idx = s.rfind(")")
+    assert left_idx != -1 and right_idx != -1, "Invalid parentheses string"
+    assert right_idx > left_idx, "Invalid parentheses string"
+    return float(s[left_idx+1: right_idx])
+
+def keep_trail_bracket_str(s) -> str:
+    """
+    Return string in the trailling bracket of a string.
+
+    e.g.
+    keep_trail_bracket_str("Marry Christmas (24)") -> Christmas
+    """
+    left_idx = s.rfind("[")
+    right_idx = s.rfind("]")
+    assert left_idx != -1 and right_idx != -1, "Invalid brackets string"
+    assert right_idx > left_idx, "Invalid brackets string"
+    return s[left_idx+1: right_idx]
