a30f067e85a3c0afeaa9511d725a9fbdec30188d
Winston Chen <chenwt@uw.edu>
2021-12-26 23:01:16 -0800

    clean up depricated scripts

--- src/data_process/run_gen_shift_requirement.sh
+++
@@ -1,5 +0,0 @@
-python3 gen_shift_requirement.py \
-    --data_name "elm_autumn_2021" \
-    --start_date "9/17/21" \
-    --end_date "12/09/21" \
-    --excluding_dates 11/24/21 11/25/21 11/26/21 11/27/21
