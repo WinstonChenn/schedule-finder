e9dfae3f68c998a7e8d873d1d916e01b7cabc130
Winston Chen <chenwt@uw.edu>
2021-12-29 10:10:25 -0800

    remove depricated files

--- src/run_main_old.sh
+++
@@ -1,6 +0,0 @@
-python3 main.py \
-    --data_dir "../../data/" \
-    --data_name "elm_autumn_2021" \
-    --max_time 3600 \
-    --holidays 11/11/21 \
-    --excluding_dates 11/24/21 11/25/21 11/26/21 11/27/21
